38227ad5489aa441a388e4c430a177348bcee6a5
Mitchell Burnett <mitcburnett@CB480-EE09731.ee.byu.edu>
2016-09-26 15:34:58 -0600

    pfb fix

--- src/pfb/pfb.cu
+++ src/pfb/pfb.cu
@@ -134,7 +134,7 @@ int loadCoeff(int iCudaDevice){
 	if(lTotCUDAMalloc > stDevProp.totalGlobalMem) {
 		(void) fprintf(stderr,
 						"ERROR: Total memory requested on GPU is %g MB of %g possible MB.\n"
-						"\tMemory break-down:\n"
+						"\t**** Memory break-down *****\n"
 						"\tInput data buffer:\t%g MB\n"
 						"\tFFT in array:\t%g MB\n"
 						"\tFFT out array:\t%g MB\n"
